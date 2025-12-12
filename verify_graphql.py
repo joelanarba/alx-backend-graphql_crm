bb762790835eda6c953b7aa3eebca502895a80cc
joelanarba <anarbajoel@gmail.com>
2025-12-12 23:31:21 +0000

    Task 0: Set Up GraphQL Endpoint

---
+++ verify_graphql.py
@@ -0,0 +1,21 @@
+import os
+import django
+import json
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_graphql_crm.settings')
+django.setup()
+
+from django.test import RequestFactory
+from graphene_django.views import GraphQLView
+
+query = '''
+{
+  hello
+}
+'''
+
+factory = RequestFactory()
+request = factory.post('/graphql', data={'query': query}, content_type='application/json')
+view = GraphQLView.as_view(graphiql=False)
+response = view(request)
+print(response.content.decode())
